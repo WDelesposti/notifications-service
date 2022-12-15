0fcbe9601b8ec59e86f7c8812e5c093027ea2c2e
Wallace Delesposti <wallace.delesposti@gmail.com>
2022-12-15 01:24:59 -0300

    feat: organization and build entities

---
+++ src/application/entities/notification.ts
@@ -0,0 +1,58 @@
+export interface NotifcationProps {
+  recipientId: string;
+  content: string;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notifcation {
+  private props: NotifcationProps;
+
+  constructor(props: NotifcationProps) {
+    this.props = props;
+  }
+
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set content(content: string) {
+    this.props.content = content;
+  }
+
+  public get content(): string {
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
+
+// const notification = new Notifcation({
+//   content: 'Hello World',
+//   category: 'default',
+// });
+
+// notification.content = 'Hello World';
